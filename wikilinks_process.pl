62f7ed417820691cbebd29bc71c4368ede57e09e
Petteri Jekunen <petteri.jekunen@tuni.fi>
2021-02-01 11:07:19 +0200

    Use quotemeta to escape special characters before regex operations. Env DEBUG_ON for switching debug_print on

--- wikilinks_process.pl
+++ wikilinks_process.pl
@@ -7,9 +7,15 @@ Usage: ./CMD.pl SRC_DIR DST_DIR
 Finds the HTML files in SRC_DIR and processes their content to the DST_DIR.
 Both anonymous and named versions.
 
+For debugging:
+$ export DEBUG_ON=1
+
 =cut
 
 use English;
+use Env qw(DEBUG_ON);
+
+#$my $DEBUG_ON = ENV('DEBUG_ON');
 
 my $src_file = $ARGV[0];
 my $dst_file = $ARGV[1];
@@ -18,12 +24,19 @@ open(FILE, '<', $src_file) or die "NO FILE";
 open(OUT, '>',  $dst_file);
 
 while (<FILE>) {
-	print $_;
+	debug_print( $_);
 	my $line = $_;
 	if ($line =~ m,(https://wiki.tamk.fi[^<> ]+),) {
+		debug_print( "WIKI https FOUND\n");
 		my $url = $1;
+		debug_print( "WIKI https FOUND, URL: $url\n");
 		if ($line !~ m,href,) {
-			$line =~ s,$1,<a href="$url">$url</a>,;
+			my $qurl = quotemeta($url);
+			debug_print( "NO HREF in line QUOTED: $qurl\n");
+			if ($line =~ m|$qurl|) {
+				debug_print( "URL FOUND $url ... subs\n");
+				$line =~ s|$qurl|<a href="$url">$url</a>|;
+				}
 		}
 	}
 	print OUT $line;
@@ -31,4 +44,8 @@ while (<FILE>) {
 
 close FILE;
 close OUT;
+
+sub debug_print {
+	$DEBUG_ON and print @_;
+}
 
